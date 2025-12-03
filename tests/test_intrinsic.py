d8de441744d38132345e0eb2c6a9df45cebab079
Jisoo Song <jeesoo9595@snu.ac.kr>
2025-12-03 13:50:00 +0900

    feat: support test with gpu

--- tests/test_intrinsic.py
+++ tests/test_intrinsic.py
@@ -9,13 +9,18 @@ np.random.seed(0)
 
 
 def test_inverse_intrinsic_pns():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
     X = circular_data()
     pns = IntrinsicPNS(n_components=2)
-    X_transformed = pns.fit_transform(X).astype(np.float32)
+    X_transformed = pns.fit_transform(X)
+    true_reconstruct = pns.inverse_transform(X_transformed)
 
-    pns_torch = InverseIntrinsicPNS(pns)
+    pns_torch = InverseIntrinsicPNS(pns).to(device)
+    X_transformed_tensor = torch.from_numpy(X_transformed.copy()).float().to(device)
+
     assert torch.allclose(
-        pns_torch.forward(torch.from_numpy(X_transformed).float()),
-        torch.from_numpy(pns.inverse_transform(X_transformed)).float(),
+        pns_torch.forward(X_transformed_tensor).cpu(),
+        torch.from_numpy(true_reconstruct).float(),
         atol=1e-3,
     )
